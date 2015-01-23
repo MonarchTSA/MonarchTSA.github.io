844fbf7ab94f1e81128664d2fb3fef44e05be306
Kyra <infinityplus42@users.noreply.github.com>
2015-01-23 09:00:58 -0700

    Made the header
    Stole code from monarchtsa.com
    yay

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<link href="../images/favicon.png" rel="icon" type="image/x-icon" />
+      <meta charset="utf-8">
+       <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+       <link rel="shortcut icon" type="image/png" href="../images/favicon.png"/>
+      <link rel="stylesheet" href="/css/main.css">
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script type="text/JavaScript" src="js/scripts.js"></script>
+   </head>
+   <body>
+       <header>
+           <div id="logo"><a href="/">MoHi Teslas: Open Source Software</a></div>
+           
+                <ul id="nav">
+               <li><a href="/index.php">Home</a></li>
+               <li><a href="/deets.php">Detailed Report</a></li>
+               <li><a href="/people.php">The Team</a></li>
+       </header>
